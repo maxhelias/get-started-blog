24ac822a9478b0ee7223a70e073c95b87194bb5f
Maxime Hélias <maxime.helias@odandb.com>
2022-03-07 11:17:41 +0100

    05-ORM

---
+++ migrations/Version20220307102018.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220307102018 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Création de la table auteur';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE author (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE author');
+    }
+}
